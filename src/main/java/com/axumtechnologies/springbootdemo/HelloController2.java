fdf238a6c762189111730ac29c6a77c27b10ff6b
Inyang Umoh <iumoh@cpxsecurity.com>
2020-10-16 07:22:23 -0700

    demo app

--- src/main/java/com/axumtechnologies/springbootdemo/HelloController2.java
+++ src/main/java/com/axumtechnologies/springbootdemo/HelloController2.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloController {
+public class HelloController2 {
 	
 	@RequestMapping("/")
 	public String index() {
